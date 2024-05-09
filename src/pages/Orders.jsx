90011ff29d2994bbd3bbb5630e21e62c7a25ddc2
Asrorbek477 <asrorbeksaidmurodov2004@gmail.com>
2024-05-09 21:41:30 +0500

    new proyekt

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,17 @@
+function Orders() {
+  return (
+    <div className="align-content">
+      <h1>Orders</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        facilis ipsam vero illum. Nam, similique! Ex similique quos deleniti
+        eligendi est nobis aperiam, ipsam laborum magnam voluptates pariatur
+        dolorem doloribus obcaecati quis voluptas quidem reprehenderit ullam
+        inventore. Aliquid, laudantium, fugiat beatae voluptatibus esse quos
+        quidem earum delectus, ex rerum est.
+      </p>
+    </div>
+  );
+}
+
+export default Orders
